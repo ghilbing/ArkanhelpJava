ecd7e23ecc91c6cb787f4220ee023d63cc210e4f
ghilbing <ghilbing@gmail.com>
2021-02-12 14:10:49 -0800

    Locale multilanguage working

--- app/src/main/java/com/hilbing/arkanhelp/MainActivity.java
+++ app/src/main/java/com/hilbing/arkanhelp/MainActivity.java
@@ -2,13 +2,37 @@ package com.hilbing.arkanhelp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //english
+        setAppLocale("es");
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private void setAppLocale(String localeCode){
+        Resources resources = getResources();
+        DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+        Configuration configuration = resources.getConfiguration();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1){
+            configuration.setLocale(new Locale(localeCode.toLowerCase()));
+        } else {
+            configuration.locale = new Locale(localeCode.toLowerCase());
+        }
+        resources.updateConfiguration(configuration, displayMetrics);
+
     }
 }
